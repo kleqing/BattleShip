f818a97ef1f5c7890480a34315beea0a5074bd69
kleqing <nhanpk28122004@gmail.com>
2025-10-19 14:00:10 +0700

    [Update] Revert to 5 ships

--- BattleShip.WebApi/Models/GameConstant.cs
+++ BattleShip.WebApi/Models/GameConstant.cs
@@ -7,7 +7,9 @@ public static class GameConstant
     public static readonly ShipDefinition[] SHIPS =
     {
         new(1, 5, "Carrier"),
-        new(2, 3, "Submarine"),
-        new(3, 2, "Destroyer")
+        new(2, 4, "Battleship"),
+        new(3, 3, "Cruiser"),
+        new(4, 3, "Submarine"),
+        new(5, 2, "Destroyer")
     };
 }
